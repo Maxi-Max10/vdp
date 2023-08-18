e5012bd56edccd9118abf112098dfe9fb80b8e1c
Maxi-Max10 <maxgiu3@gmail.com>
2023-08-18 14:19:44 -0300

    vdp

---
+++ assets/views/nav.php
@@ -0,0 +1,117 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="generator" content="Mobirise v5.8.14, mobirise.com">
+    <meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1">
+    <link rel="shortcut icon" href="assets/images/vdp-isologotipo-final-10-298x298.png" type="image/x-icon">
+    <meta name="description" content="">
+
+
+    <title>Vivacs del Plata | <?php print $campo ?></title>
+    <link rel="stylesheet" href="assets2/css/normalize.css">
+    <link rel="stylesheet" href="assets/web/assets/mobirise-icons2/mobirise2.css">
+    <link rel="stylesheet" href="assets/bootstrap/css/bootstrap.min.css">
+    <link rel="stylesheet" href="assets/bootstrap/css/bootstrap-grid.min.css">
+    <link rel="stylesheet" href="assets/bootstrap/css/bootstrap-reboot.min.css">
+    <link rel="stylesheet" href="assets/parallax/jarallax.css">
+    <link rel="stylesheet" href="assets/animatecss/animate.css">
+    <link rel="stylesheet" href="assets/dropdown/css/style.css">
+    <link rel="stylesheet" href="assets/socicon/css/styles.css">
+    <link rel="stylesheet" href="assets/theme/css/style.css">
+    <link rel="stylesheet" href="assets2/css/style.css">
+    <link href="assets/fonts/style.css" rel="stylesheet">
+    <link rel="preload" as="style" href="assets/mobirise/css/mbr-additional.css">
+    <link rel="stylesheet" href="assets/mobirise/css/mbr-additional.css" type="text/css">
+
+</head>
+
+<body>
+    <!-- style="margin-top: -32px;" -->
+    <section data-bs-version="5.1" class="menu menu2 cid-tL7nLROMV3" once="menu" id="menu2-1c"
+        style="margin-top: -32px;">
+
+        <nav class="navbar navbar-dropdown navbar-fixed-top collapsed">
+            <div class="container mb-5">
+                <div class="col-auto me-auto mt-5 mb-0">
+                    <span class="navbar-logo">
+                        <a href="page3.php">
+                            <img src="assets/images/vdp-isologotipo-final-10-298x298.png" alt=""
+                                style="height: 5rem; z-index: 3;">
+                        </a>
+                    </span>
+                </div>
+                <ul class="nav col-auto mb-2 justify-content-center mb-md-0">
+                    <li class="nav-item"><a href="#" class="nav-link px-2 link-light tes">ES</a></li>
+                    <li class="nav-link px-2 link-light tes" style="top: -6px;">|</li>
+                    <li class="nav-item"><a href="page3.php" class="nav-link px-2 link-light tes">EN</a></li>
+                    <li class="nav-item"><a href="page4.php" class="nav-link px-2 link-light tes">Contact</a></li>
+                    <li class="ms-2"> <button class="navbar-toggler" type="button" data-toggle="collapse"
+                            data-bs-toggle="collapse" data-target="#navbarSupportedContent"
+                            data-bs-target="#navbarSupportedContent" aria-controls="navbarNavAltMarkup"
+                            aria-expanded="false" aria-label="Toggle navigation">
+                            <div class="hamburger">
+                                <span></span>
+                                <span></span>
+                                <span></span>
+                                <span></span>
+                            </div>
+                        </button>
+                    </li>
+                </ul>
+                <div class="collapse navbar-collapse" id="navbarSupportedContent"
+                    aria-labelledby="navbarSupportedContent">
+                    <div class="navbar-nav nav-dropdown" data-app-modern-menu="true">
+
+                        <div class="row fuente-Vidaloka fs-2 fw-normal justify-content-around mt-5"
+                            data-app-modern-menu="true">
+                            <div class="col-4">
+                                <ul class="">
+                                    <li><a href="page8.php" class="text-decoration-none link-light">Our Story</a></li>
+                                    <li><a href="page6.php" class="text-decoration-none link-light">All-Inclusive
+                                            Experience</a></li>
+                                    <li><a href="page1.php" class="text-decoration-none link-light">Getting Here</a>
+                                    </li>
+                                    <li><a href="Page7.php" class="text-decoration-none link-light">Trip
+                                            Enhancements</a></li>
+                                </ul>
+                            </div>
+
+                            <div class="col-4">
+                                <ul class="list-unstyled">
+                                    <li><a href="page5.php" class="text-decoration-none link-light">Gallery</a></li>
+                                    <li><a href="page2.php" class="text-decoration-none link-light">FAQ </a></li>
+                                    <li><a href="page4.php" class="text-decoration-none link-light">Contact</a></li>
+                                </ul>
+                                <div class="social-row display-5 nav col-md-4 justify-content-start ">
+                                    <div class="soc-item ms-3">
+                                        <a href="" target="_blank">
+                                            <span class="mbr-iconfont socicon socicon-whatsapp"
+                                                style="color: rgb(250, 250, 250); fill: rgb(250, 250, 250);font-size: 12pt"></span>
+                                        </a>
+                                    </div>
+                                    <div class="soc-item ms-3">
+                                        <a href="" target="_blank">
+                                            <span class="mbr-iconfont socicon socicon-tripadvisor"
+                                                style="color: rgb(250, 250, 250); fill: rgb(250, 250, 250);font-size: 16pt"></span>
+                                        </a>
+                                    </div>
+                                    <div class="soc-item ms-3">
+                                        <a href="" target="_blank">
+                                            <span class="mbr-iconfont mbr-iconfont-social socicon-instagram socicon"
+                                                style="color: rgb(250, 250, 250); fill: rgb(250, 250, 250);font-size: 12pt"></span>
+                                        </a>
+                                    </div>
+
+                                </div>
+
+                            </div>
+                        </div>
+                    </div>
+                </div><!-- EndMenu -->
+            </div>
+
+        </nav>
+    </section>
---
+++ page2.php
@@ -0,0 +1,144 @@
+
+
+<?php 
+$campo = "Frequently Asked Questions";
+include "assets/views/nav.php";
+
+
+
+?>
+  
+  <section data-bs-version="5.1" class="header4 cid-tL7jvAGA4r mbr-fullscreen mbr-parallax-background" id="header4-18">
+
+    
+    <div class="mbr-overlay"></div>
+    <div class="container">
+        <div class="row">
+            <div class="content-wrap">
+                <h1 class="mbr-section-title mbr-fonts-style mbr-white mb-3 display-1"><strong>Frequently Asked Questions</strong></h1>
+                
+                <p class="mbr-fonts-style mbr-text mbr-white mb-3 display-7">
+                    Learn more! Here are some common questions about Vivacs del Plata.
+                </p>
+
+                
+            </div>
+        </div>
+    </div>
+</section>
+
+<section data-bs-version="5.1" class="content16 cid-tL7jvB3Ksf" id="content16-19">
+
+    
+
+    
+    
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-12 col-md-9">
+                
+                <div id="bootstrap-accordion_1" class="panel-group accordionStyles accordion" role="tablist" aria-multiselectable="true">
+                    <div class="card">
+                        <div class="card-header" role="tab" id="headingOne">
+                            
+                                <h6 class="panel-title-edit mbr-fonts-style mb-0 display-7"><strong>FAQ</strong></h6>
+                                <span class="sign mbr-iconfont mbri-arrow-down"></span>
+                            
+                        </div>
+                        
+                    </div>
+                    <div class="card mb-3">
+                        <div class="card-header" role="tab" id="headingOne">
+                            <a role="button" class="panel-title collapsed" data-toggle="collapse" data-bs-toggle="collapse" data-core="" href="#collapse2_1" aria-expanded="false" aria-controls="collapse2">
+                                <h6 class="panel-title-edit mbr-fonts-style mb-0 display-7"><strong>How does the transfer service operate?</strong></h6>
+                                <span class="sign mbr-iconfont mbri-arrow-down"></span>
+                            </a>
+                        </div>
+                        <div id="collapse2_1" class="panel-collapse noScroll collapse" role="tabpanel" aria-labelledby="headingOne" data-parent="#accordion" data-bs-parent="#bootstrap-accordion_1">
+                            <div class="panel-body">
+                                <p class="mbr-fonts-style panel-text display-4">
+                                    The transfer service covers trips from the airport, downtown Mendoza, or Valle de Uco to Vivacs del Plata, as well as the return journey, all at a flat rate.</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="card mb-3">
+                        <div class="card-header" role="tab" id="headingOne">
+                            <a role="button" class="panel-title collapsed" data-toggle="collapse" data-bs-toggle="collapse" data-core="" href="#collapse3_1" aria-expanded="false" aria-controls="collapse3">
+                                <h6 class="panel-title-edit mbr-fonts-style mb-0 display-7"><strong>What is the capacity for the sailing excursion?</strong></h6>
+                                <span class="sign mbr-iconfont mbri-arrow-down"></span>
+                            </a>
+                        </div>
+                        <div id="collapse3_1" class="panel-collapse noScroll collapse" role="tabpanel" aria-labelledby="headingOne" data-parent="#accordion" data-bs-parent="#bootstrap-accordion_1">
+                            <div class="panel-body">
+                                <p class="mbr-fonts-style panel-text display-4">
+                                    Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ullam, labore repellendus quae minima incidunt id eaque facilis molestias soluta autem temporibus distinctio explicabo pariatur ab assumenda magni quod ad voluptate!</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="card mb-3">
+                        <div class="card-header" role="tab" id="headingOne">
+                            <a role="button" class="panel-title collapsed" data-toggle="collapse" data-bs-toggle="collapse" data-core="" href="#collapse4_1" aria-expanded="false" aria-controls="collapse4">
+                                <h6 class="panel-title-edit mbr-fonts-style mb-0 display-7"><strong>What is the weather like?</strong></h6>
+                                <span class="sign mbr-iconfont mbri-arrow-down"></span>
+                            </a>
+                        </div>
+                        <div id="collapse4_1" class="panel-collapse noScroll collapse" role="tabpanel" aria-labelledby="headingOne" data-parent="#accordion" data-bs-parent="#bootstrap-accordion_1">
+                            <div class="panel-body">
+                                <p class="mbr-fonts-style panel-text display-4">
+                                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero quis, nam voluptatem quisquam impedit dolore dolores sequi vitae saepe neque nemo a asperiores autem deleniti ex ut natus aliquam sunt.</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="card mb-3">
+                        <div class="card-header" role="tab" id="headingOne">
+                            <a role="button" class="panel-title collapsed" data-toggle="collapse" data-bs-toggle="collapse" data-core="" href="#collapse5_1" aria-expanded="false" aria-controls="collapse5">
+                                <h6 class="panel-title-edit mbr-fonts-style mb-0 display-7"><strong>What does the all-inclusive experience include?</strong></h6>
+                                <span class="sign mbr-iconfont mbri-arrow-down"></span>
+                            </a>
+                        </div>
+                        <div id="collapse5_1" class="panel-collapse noScroll collapse" role="tabpanel" aria-labelledby="headingOne" data-parent="#accordion" data-bs-parent="#bootstrap-accordion_1">
+                            <div class="panel-body">
+                                <p class="mbr-fonts-style panel-text display-4">
+                                    Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptates aliquid, maxime tempore vero recusandae nobis nulla quasi itaque distinctio inventore velit nisi maiores at! Iure nulla sint deserunt molestias placeat?</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="card mb-3">
+                        <div class="card-header" role="tab" id="headingOne">
+                            <a role="button" class="panel-title collapsed" data-toggle="collapse" data-bs-toggle="collapse" data-core="" href="#collapse6_1" aria-expanded="false" aria-controls="collapse6">
+                                <h6 class="panel-title-edit mbr-fonts-style mb-0 display-7"><strong>What is the overnight capacity of the inn?</strong></h6>
+                                <span class="sign mbr-iconfont mbri-arrow-down"></span>
+                            </a>
+                        </div>
+                        <div id="collapse6_1" class="panel-collapse noScroll collapse" role="tabpanel" aria-labelledby="headingOne" data-parent="#accordion" data-bs-parent="#bootstrap-accordion_1">
+                            <div class="panel-body">
+                                <p class="mbr-fonts-style panel-text display-4">
+                                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit iusto doloremque dolorum sunt consequuntur vel delectus, nihil voluptas suscipit molestias beatae, quae rem. Nam ipsam, dolorem totam nisi sit eius.</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+<section data-bs-version="5.1" class="content5 cid-tL7jvBG780" id="content5-1a">
+    
+    <div class="container mt-4 mb-4">
+        <div class="row justify-content-center">
+            <div class="col-md-12 col-lg-9">
+                
+                <h4 class="mbr-section-subtitle mbr-fonts-style mb-4 display-1">Still need help?</h4>
+                <p class="mbr-text mbr-fonts-style display-7">
+                    For any other questions, please contact us here or write us at <strong> vivacsdelplata@gmail.com </strong></p>
+            </div>
+        </div>
+    </div>
+</section>
+
+<?php
+
+include "assets/views/footer.php"
+
+?>

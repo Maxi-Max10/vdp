710c20012cbebf1b3ae9f6ddb19f7ea8c70191a9
Maxi-Max10 <maxgiu3@gmail.com>
2023-08-29 09:21:41 -0300

    footer

--- page6.php
+++ page6.php
@@ -905,67 +905,6 @@ include "assets/views/nav.php";
 </section>
 
 
-    
-    <section data-bs-version="5.1" class="footer4 cid-tMlWvWDpDR" once="footers" id="footer4-24">
-        
-        <div class="mbr-overlay" style="opacity: 1.1; background-color: rgb(35, 35, 35);"></div>   
-        <div class="container">
-            <div class="row mbr-white mb-3">
-                <div class="col-6 col-lg-3">
-                    <div class="media-wrap col-md-8 col-12">
-                    <a href="">
-                        <img src="assets/images/vdp-isologotipo-final-10-337x337.png" alt="">
-                    </a>
-                </div>
-            </div>
-            <div class="col-12 col-md-6 col-lg-3">
-                <h5 class="mbr-section-subtitle mbr-fonts-style mb-2 display-7"></h5>
-                <p class="mbr-text mbr-fonts-style mb-4 display-4"></p>
-                
-            </div>
-            <div class="col-12 col-md-6 col-lg-3">
-                <h5 class="mbr-section-subtitle mbr-fonts-style mb-2 display-7"></h5>
-                <ul class="list mbr-fonts-style display-5">
-                    <a href="page5.php"></a><li class="mbr-text item-wrap">Gallery</li></a>
-                    <a href="page2.php"><li class="mbr-text item-wrap">FAQ</li></a>
-                    <a href="page4.php"></a><li class="mbr-text item-wrap">Contact</li></a>
-                </ul>
-            </div>
-            <div class="col-12 col-md-6 col-lg-3">
-                <h5 class="mbr-section-subtitle mbr-fonts-style mb-2 display-7"></h5>
-                <div class="social-row display-7">
-                    <div class="soc-item">
-                        <a href="" target="_blank">
-                            <span class="mbr-iconfont socicon socicon-whatsapp" style="color: rgb(250, 250, 250); fill: rgb(250, 250, 250);"></span>
-                        </a>
-                    </div>
-                    <div class="soc-item">
-                        <a href="" target="_blank">
-                            <span class="mbr-iconfont socicon socicon-tripadvisor" style="color: rgb(250, 250, 250); fill: rgb(250, 250, 250);"></span>
-                        </a>
-                    </div>
-                    <div class="soc-item">
-                        <a href="" target="_blank">
-                            <span class="mbr-iconfont mbr-iconfont-social socicon-instagram socicon"
-                                        style="color: rgb(250, 250, 250); fill: rgb(250, 250, 250);"></span>
-                        </a>
-                    </div>
-                </div>              
-
-            </div>
-            <div class="mt-5 mb-4" style="height: 1px; background-color: white; text-align: center">
-            <span style="background-color: white; position: relative; top: -0.5em;">
-                
-            </span>
-            </div>
-            <div class="col-md-6 copyright">
-                <p class="mbr-text mbr-fonts-style display-4">
-                &copy;<script>document.write(new Date().getFullYear());</script> Vivacs del Plata- All Rights Reserved&nbsp; &nbsp; Terms &amp; Conditions&nbsp;
-                    &nbsp; Privacy Policy</p>
-            </div>
-        </div>
-    </div>
-</section>
 
 <?php
 

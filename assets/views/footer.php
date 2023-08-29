8e3fa7103bc72412cfb987389262005c77d20472
Agustin Alejandro Sosa <sagustin2001@gmail.com>
2023-08-28 21:27:23 -0300

    swiper script

--- assets/views/footer.php
+++ assets/views/footer.php
@@ -81,7 +81,7 @@
 <script src="assets2/js/options.js"></script>
 <script src="https://unpkg.com/swiper/swiper-bundle.min.js"></script>
 <script src="js/scriptL.js"></script>
-
+<script src="js/swiper-script.js"></script>
 <!--    SCRIPTS AÑADIDOS    -->    
 <script> //SCRIPT 
      function scrollToTop() {
--- page8.php
+++ page8.php
@@ -227,49 +227,6 @@ include "assets/views/nav.php";
         </div>
     </div>
 </section>
-
-<script>
-    const swiper = new Swiper('#swiper', {
-        speed: 400,
-        spaceBetween: 100,
-        loop: true, // Permite el bucle continuo del carrusel
-      autoplay: {
-        delay: 3000, // Cambia la diapositiva cada 3 segundos
-      },
-      disableOnInteraction:false,
-      allowTouchMove:false,
-    });
-    const swiper2 = new Swiper('#swiper2', {
-        speed: 400,
-        spaceBetween: 100,
-        loop: true, // Permite el bucle continuo del carrusel
-      autoplay: {
-        delay: 5000, // Cambia la diapositiva cada 3 segundos
-      },
-      disableOnInteraction:false,
-      allowTouchMove:false,
-    });
-    const swiper3 = new Swiper('#swiper3', {
-        speed: 400,
-        spaceBetween: 100,
-        loop: true, // Permite el bucle continuo del carrusel
-      autoplay: {
-        delay: 4000, // Cambia la diapositiva cada 3 segundos
-      },
-      disableOnInteraction:false,
-      allowTouchMove:false,
-    });
-    const swiper4 = new Swiper('#swiper4', {
-        speed: 400,
-        spaceBetween: 100,
-        loop: true, // Permite el bucle continuo del carrusel
-      autoplay: {
-        delay: 6000, // Cambia la diapositiva cada 3 segundos
-      },
-      disableOnInteraction:false,
-      allowTouchMove:false,
-    });
-</script>
 <script>
     var boton = document.getElementById("cardTitle");
     var textoDesplegable = document.getElementById("textoDesplegable");

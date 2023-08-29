c024063b323049c1f830886b9c18cb4149c505cf
Maxi-Max10 <maxgiu3@gmail.com>
2023-08-29 10:19:24 -0300

    nav

--- assets/views/nav.php
+++ assets/views/nav.php
@@ -9,53 +9,38 @@
     <link rel="shortcut icon" href="assets/images/vdp-isologotipo-final-10-298x298.png" type="image/x-icon">
     <meta name="description" content="">
 
-<title>Vivacs del Plata | <?php print $campo ?></title>
-<link rel="stylesheet" href="assets/css/normalize.css">
-<link rel="stylesheet" href="assets/web/assets/mobirise-icons2/mobirise2.css">
-<link rel="stylesheet" href="assets/web/assets/mobirse-icons/mobirise-icons.css">
-<link rel="stylesheet" href="assets/bootstrap/css/bootstrap.min.css">
-<link rel="stylesheet" href="assets/bootstrap/css/bootstrap-grid.min.css">
-<link rel="stylesheet" href="assets/bootstrap/css/bootstrap-reboot.min.css">
-<link rel="stylesheet" href="assets/parallax/jarallax.css">
-<link rel="stylesheet" href="assets/animatecss/animate.css">
-<link rel="stylesheet" href="assets/dropdown/css/style.css">
-<link rel="stylesheet" href="assets/socicon/css/styles.css">
-<link rel="stylesheet" href="assets/theme/css/style.css">
-<link rel="stylesheet" href="assets/styleju/stylej.css">
-<link rel="stylesheet" href="assets/css/style.css">
-<link rel="stylesheet" href="assets/css/page7.css">
-<link rel="stylesheet" href="assets/css/page4.css">
-<link rel="stylesheet" href="assets/css/page6.css">
-<link rel="stylesheet" href="assets/css/page8.css">
-<link rel="stylesheet" href="assets/fonts/Roboto-Medium/style.css">
-<link rel="stylesheet" href="assets/fonts/Roboto-Bold/style.css">
-<link href="assets/fonts/style.css" rel="stylesheet">
-<link rel="preload" as="style" href="assets/mobirise/css/mbr-additional.css">
-<link rel="stylesheet" href="assets/mobirise/css/mbr-additional.css" type="text/css">
-<link href="https://cdn.jsdelivr.net/npm/swiffy-slider@1.6.0/dist/css/swiffy-slider.min.css" rel="stylesheet" crossorigin="anonymous">
-<link rel="stylesheet" href="assets/css/page8.css">
-<link href="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/css/select2.min.css" rel="stylesheet" />
-<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@10/swiper-bundle.min.css"/>
-<!-- <link rel="stylesheet" href="index-2.css" />ESTE LINK ARMA DESORDEN EN LAS PAGE 2-1 -->
-<link
-      rel="stylesheet"
-      type="text/css"
-      href="//cdn.jsdelivr.net/gh/kenwheeler/slick@1.8.1/slick/slick.css"
-    />
-    <link
-      rel="stylesheet"
-      type="text/css"
-      href="//cdn.jsdelivr.net/gh/kenwheeler/slick@1.8.1/slick/slick-theme.css"
-    />
-    <link
-      rel="stylesheet"
-      href="https://cdn.jsdelivr.net/npm/bootstrap-icons/font/bootstrap-icons.css"
-    />
-    <link
-      href="https://cdn.jsdelivr.net/npm/swiffy-slider@1.6.0/dist/css/swiffy-slider.min.css"
-      rel="stylesheet"
-      crossorigin="anonymous"
-    />
+    <title>Vivacs del Plata | <?php print $campo ?></title>
+    <link rel="stylesheet" href="assets/css/normalize.css">
+    <link rel="stylesheet" href="assets/web/assets/mobirise-icons2/mobirise2.css">
+    <link rel="stylesheet" href="assets/web/assets/mobirse-icons/mobirise-icons.css">
+    <link rel="stylesheet" href="assets/bootstrap/css/bootstrap.min.css">
+    <link rel="stylesheet" href="assets/bootstrap/css/bootstrap-grid.min.css">
+    <link rel="stylesheet" href="assets/bootstrap/css/bootstrap-reboot.min.css">
+    <link rel="stylesheet" href="assets/parallax/jarallax.css">
+    <link rel="stylesheet" href="assets/animatecss/animate.css">
+    <link rel="stylesheet" href="assets/dropdown/css/style.css">
+    <link rel="stylesheet" href="assets/socicon/css/styles.css">
+    <link rel="stylesheet" href="assets/theme/css/style.css">
+    <link rel="stylesheet" href="assets/styleju/stylej.css">
+    <link rel="stylesheet" href="assets/css/style.css">
+    <link rel="stylesheet" href="assets/css/page7.css">
+    <link rel="stylesheet" href="assets/css/page4.css">
+    <link rel="stylesheet" href="assets/css/page6.css">
+    <link rel="stylesheet" href="assets/css/page8.css">
+    <link rel="stylesheet" href="assets/fonts/Roboto-Medium/style.css">
+    <link rel="stylesheet" href="assets/fonts/Roboto-Bold/style.css">
+    <link href="assets/fonts/style.css" rel="stylesheet">
+    <link rel="preload" as="style" href="assets/mobirise/css/mbr-additional.css">
+    <link rel="stylesheet" href="assets/mobirise/css/mbr-additional.css" type="text/css">
+    <link href="https://cdn.jsdelivr.net/npm/swiffy-slider@1.6.0/dist/css/swiffy-slider.min.css" rel="stylesheet" crossorigin="anonymous">
+    <link rel="stylesheet" href="assets/css/page8.css">
+    <link href="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/css/select2.min.css" rel="stylesheet" />
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@10/swiper-bundle.min.css" />
+    <!-- <link rel="stylesheet" href="index-2.css" />ESTE LINK ARMA DESORDEN EN LAS PAGE 2-1 -->
+    <link rel="stylesheet" type="text/css" href="//cdn.jsdelivr.net/gh/kenwheeler/slick@1.8.1/slick/slick.css" />
+    <link rel="stylesheet" type="text/css" href="//cdn.jsdelivr.net/gh/kenwheeler/slick@1.8.1/slick/slick-theme.css" />
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons/font/bootstrap-icons.css" />
+    <link href="https://cdn.jsdelivr.net/npm/swiffy-slider@1.6.0/dist/css/swiffy-slider.min.css" rel="stylesheet" crossorigin="anonymous" />
     <link rel="stylesheet" href="assets/css/page6.css">
 
 </head>
@@ -68,21 +53,17 @@
                 <div class="col-auto me-auto mt-5 mb-0">
                     <span class="navbar-logo">
                         <a href="index.php">
-                            <img id="img-logo" class="" src="assets/images/vdp-isologotipo-final-10-298x298.png" alt=""
-                                style="height: 5rem; z-index: 3;">
+                            <img id="img-logo" class="" src="assets/images/vdp-isologotipo-final-10-298x298.png" alt="" style="height: 5rem; z-index: 3;">
                         </a>
                     </span>
                 </div>
                 <ul class="nav col-auto mb-2 justify-content-center mb-md-0 mt-0 mt-md-4">
-                        <li id="closeLi" class="nav-item hidden"><a id="close" class="nav-link px-2 link-light tes text-nav">CLOSE</a></li>    
-                        <li id="li1" class="nav-item"><a id="tes-1" href="#" class="nav-link px-2 link-light tes text-nav">ES</a></li>
-                        <li id="separator" class="nav-link px-2 link-light tes text-nav" style="top: -6px;">|</li>
-                        <li id="li2" class="nav-item"><a id="tes-2" href="#" class="nav-link px-2 link-light tes text-nav">EN</a></li>
-                        <li id="li3" class="nav-item"><a id="tes-3" href="page4.php" class="nav-link px-2 link-light tes text-nav">CONTACT</a></li>
-                    <li id="burger" class="ms-2"> <button class="navbar-toggler" type="button" data-toggle="collapse"
-                            data-bs-toggle="collapse" data-target="#navbarSupportedContent"
-                            data-bs-target="#navbarSupportedContent" aria-controls="navbarNavAltMarkup"
-                            aria-expanded="false" aria-label="Toggle navigation">
+                    <li id="closeLi" class="nav-item hidden"><a id="close" class="nav-link px-2 link-light tes text-nav">CLOSE</a></li>
+                    <li id="li1" class="nav-item"><a id="tes-1" href="#" class="nav-link px-2 link-light tes text-nav">ES</a></li>
+                    <li id="separator" class="nav-link px-2 link-light tes text-nav" style="top: -6px;">|</li>
+                    <li id="li2" class="nav-item"><a id="tes-2" href="#" class="nav-link px-2 link-light tes text-nav">EN</a></li>
+                    <li id="li3" class="nav-item"><a id="tes-3" href="page4.php" class="nav-link px-2 link-light tes text-nav">CONTACT</a></li>
+                    <li id="burger" class="ms-2"> <button class="navbar-toggler" type="button" data-toggle="collapse" data-bs-toggle="collapse" data-target="#navbarSupportedContent" data-bs-target="#navbarSupportedContent" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
                             <div class="hamburger">
                                 <span id="burger-1" class="tes-2"></span>
                                 <span id="burger-2" class="tes-2"></span>
@@ -92,12 +73,10 @@
                         </button>
                     </li>
                 </ul>
-                <div class="collapse navbar-collapse" id="navbarSupportedContent"
-                    aria-labelledby="navbarSupportedContent">
+                <div class="collapse navbar-collapse" id="navbarSupportedContent" aria-labelledby="navbarSupportedContent">
                     <div class="navbar-nav nav-dropdown" data-app-modern-menu="true">
-        
-                        <div class="fade-slide-down row fuente-Vidaloka fs-2 fw-normal justify-content-around mt-5"
-                            data-app-modern-menu="true">
+
+                        <div class="fade-slide-down row fuente-Vidaloka fs-2 fw-normal justify-content-around mt-5" data-app-modern-menu="true">
                             <div class="col-7">
                                 <ul class="no-bullets dropdown-transition">
                                     <li class="fs-1 dropdown-transition vidaloka-nav"><a href="page8.php" class="text-decoration-none link-light" data-section="nav" data-value="ourStory">Our Story</a></li>
@@ -109,7 +88,7 @@
                                             Enhancements</a></li>
                                 </ul>
                             </div>
-        
+
                             <div class="col-5">
                                 <ul class="list-unstyled dropdown-transition">
                                     <li class="fs-1 dropdown-transition"><a href="page5.php" class="text-decoration-none link-light vidaloka-nav" data-section="nav" data-value="gallery">Gallery</a></li>
@@ -119,20 +98,17 @@
                                 <div class="social-row display-5 nav col-md-5 justify-content-start ">
                                     <div class="soc-item ms-3">
                                         <a href="" target="_blank">
-                                            <span class="mbr-iconfont socicon socicon-whatsapp"
-                                                style="color: rgb(250, 250, 250); fill: rgb(250, 250, 250);font-size: 12pt"></span>
+                                            <span class="mbr-iconfont socicon socicon-whatsapp" style="color: rgb(250, 250, 250); fill: rgb(250, 250, 250);font-size: 12pt"></span>
                                         </a>
                                     </div>
                                     <div class="soc-item ms-3">
                                         <a href="" target="_blank">
-                                            <span class="mbr-iconfont socicon socicon-tripadvisor"
-                                                style="color: rgb(250, 250, 250); fill: rgb(250, 250, 250);font-size: 16pt"></span>
+                                            <span class="mbr-iconfont socicon socicon-tripadvisor" style="color: rgb(250, 250, 250); fill: rgb(250, 250, 250);font-size: 16pt"></span>
                                         </a>
                                     </div>
                                     <div class="soc-item ms-3">
                                         <a href="" target="_blank">
-                                            <span class="mbr-iconfont mbr-iconfont-social socicon-instagram socicon"
-                                                style="color: rgb(250, 250, 250); fill: rgb(250, 250, 250);font-size: 12pt"></span>
+                                            <span class="mbr-iconfont mbr-iconfont-social socicon-instagram socicon" style="color: rgb(250, 250, 250); fill: rgb(250, 250, 250);font-size: 12pt"></span>
                                         </a>
                                     </div>
                                     <div class="soc-item ms-3">
@@ -141,12 +117,12 @@
                                         </a>
                                     </div>
                                 </div>
-        
+
                             </div>
                         </div>
                     </div>
                 </div><!-- EndMenu -->
             </div>
-        
+
         </nav>
-        </section>
+    </section>

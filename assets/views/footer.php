8fe21a698165afd7c29756d1bfd87614397f293e
Agustin Alejandro Sosa <sagustin2001@gmail.com>
2023-08-28 21:37:54 -0300

    add script nav

--- assets/views/footer.php
+++ assets/views/footer.php
@@ -82,74 +82,9 @@
 <script src="https://unpkg.com/swiper/swiper-bundle.min.js"></script>
 <script src="js/scriptL.js"></script>
 <script src="js/swiper-script.js"></script>
-
+<script src="js/the-ranch-script.js"></script>
+<script src="navbar-script.js"></script>
 <!--    SCRIPTS AÑADIDOS    -->    
-<script> //SCRIPT 
-     function scrollToTop() {
-      window.scrollTo(0, 0);
-      document.getElementById("nav").style.top = "0"
-    }
-    window.onload = scrollToTop;
-</script>
-<script> //NAVBAR SCRIPT
-    const hamburgerButton = document.getElementById('burger');
-    const closeText = document.getElementById('close')
-    const closeLi = document.getElementById('closeLi');
-    const li1 = document.getElementById('li1');
-    const li2 = document.getElementById('li2');
-    const li3 = document.getElementById('li3');
-    const logo = document.getElementById('img-logo');
-    const es = document.getElementById('tes-1');
-    const en = document.getElementById('tes-2');
-    const separator = document.getElementById('separator');
-    const contact = document.getElementById('tes-3');
-    const burger1 = document.getElementById('burger-1');
-    const burger2 = document.getElementById('burger-2');
-    const burger3 = document.getElementById('burger-3');
-    const burger4 = document.getElementById('burger-4');
-
-    hamburgerButton.addEventListener('click', () => {
-    separator.classList.toggle('hidden');
-    li1.classList.toggle('hidden');
-    li2.classList.toggle('hidden');
-    li3.classList.toggle('hidden');
-    closeLi.classList.toggle('hidden');
-    logo.classList.toggle('img-color');
-    closeText.classList.toggle('white-text');
-    es.classList.toggle('white-text');
-    en.classList.toggle('white-text');
-    separator.classList.toggle('white-text');
-    burger1.classList.toggle('white-burger');
-    burger2.classList.toggle('white-burger');
-    burger3.classList.toggle('white-burger');
-    burger4.classList.toggle('white-burger');
-    contact.classList.toggle('white-text');
-    closeText.classList.toggle('tes');
-    es.classList.toggle('tes');
-    en.classList.toggle('tes');
-    separator.classList.toggle('tes');
-    contact.classList.toggle('tes');
-    burger1.classList.toggle('tes-2');
-    burger2.classList.toggle('tes-2');
-    burger3.classList.toggle('tes-2');
-    burger4.classList.toggle('tes-2');
-    });
-</script>
-<script> // NAVBAR SCRIPT 
-    let prevScrollPos = window.pageYOffset;
-
-    window.addEventListener("scroll", function () {
-    let currentScrollPos = window.pageYOffset;
-
-    if (prevScrollPos > currentScrollPos) {
-        document.getElementById("nav").style.top = "0";
-    } else {
-        document.getElementById("nav").style.top = "-120px"; // Altura de la barra
-    }
-
-    prevScrollPos = currentScrollPos;
-    });
-</script>
-  <input name="animation" type="hidden">
-  </body>
+<input name="animation" type="hidden">
+</body>
 </html>
---
+++ js/navbar-script.js
@@ -0,0 +1,57 @@
+const hamburgerButton = document.getElementById('burger');
+    const closeText = document.getElementById('close')
+    const closeLi = document.getElementById('closeLi');
+    const li1 = document.getElementById('li1');
+    const li2 = document.getElementById('li2');
+    const li3 = document.getElementById('li3');
+    const logo = document.getElementById('img-logo');
+    const es = document.getElementById('tes-1');
+    const en = document.getElementById('tes-2');
+    const separator = document.getElementById('separator');
+    const contact = document.getElementById('tes-3');
+    const burger1 = document.getElementById('burger-1');
+    const burger2 = document.getElementById('burger-2');
+    const burger3 = document.getElementById('burger-3');
+    const burger4 = document.getElementById('burger-4');
+
+    hamburgerButton.addEventListener('click', () => {
+    separator.classList.toggle('hidden');
+    li1.classList.toggle('hidden');
+    li2.classList.toggle('hidden');
+    li3.classList.toggle('hidden');
+    closeLi.classList.toggle('hidden');
+    logo.classList.toggle('img-color');
+    closeText.classList.toggle('white-text');
+    es.classList.toggle('white-text');
+    en.classList.toggle('white-text');
+    separator.classList.toggle('white-text');
+    burger1.classList.toggle('white-burger');
+    burger2.classList.toggle('white-burger');
+    burger3.classList.toggle('white-burger');
+    burger4.classList.toggle('white-burger');
+    contact.classList.toggle('white-text');
+    closeText.classList.toggle('tes');
+    es.classList.toggle('tes');
+    en.classList.toggle('tes');
+    separator.classList.toggle('tes');
+    contact.classList.toggle('tes');
+    burger1.classList.toggle('tes-2');
+    burger2.classList.toggle('tes-2');
+    burger3.classList.toggle('tes-2');
+    burger4.classList.toggle('tes-2');
+    });
+
+
+    let prevScrollPos = window.pageYOffset;
+
+    window.addEventListener("scroll", function () {
+    let currentScrollPos = window.pageYOffset;
+
+    if (prevScrollPos > currentScrollPos) {
+        document.getElementById("nav").style.top = "0";
+    } else {
+        document.getElementById("nav").style.top = "-120px"; // Altura de la barra
+    }
+
+    prevScrollPos = currentScrollPos;
+    });

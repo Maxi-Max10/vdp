6a301e304a692a187890787c626a1c5585a1e92d
Agustin Alejandro Sosa <sagustin2001@gmail.com>
2023-08-30 17:55:12 -0300

    nav responsive

--- nav2.php
+++ nav2.php
@@ -75,23 +75,23 @@
                     <div class="navbar-nav nav-dropdown" data-app-modern-menu="true">
 
                         <div class="fade-slide-down row fuente-Vidaloka fs-2 fw-normal justify-content-around mt-5" data-app-modern-menu="true">
-                            <div class="col-7">
+                            <div class="col-12 col-sm-7">
                                 <ul class="no-bullets dropdown-transition">
-                                    <li id="nav-href-1" class="fs-1 dropdown-transition vidaloka-nav"><a href="page8.php" class="text-decoration-none link-light" data-section="nav" data-value="ourStory">Our Story</a></li>
-                                    <li id="nav-href-2" class="fs-1 dropdown-transition"><a href="page6.php" class="text-decoration-none link-light all-inclusive vidaloka-nav">All-Inclusive
+                                    <li id="nav-href-1" class="fs-3 fs-sm-1 dropdown-transition vidaloka-nav"><a href="page8.php" class="text-decoration-none link-light" data-section="nav" data-value="ourStory">Our Story</a></li>
+                                    <li id="nav-href-2" class="fs-3 fs-sm-1 dropdown-transition"><a href="page6.php" class="text-decoration-none link-light all-inclusive vidaloka-nav">All-Inclusive
                                             Experience</a></li>
-                                    <li id="nav-href-3" class="fs-1 dropdown-transition"><a href="page1.php" class="text-decoration-none link-light vidaloka-nav">Getting Here</a>
+                                    <li id="nav-href-3" class="fs-3 fs-sm-1 dropdown-transition"><a href="page1.php" class="text-decoration-none link-light vidaloka-nav">Getting Here</a>
                                     </li>
-                                    <li id="nav-href-4" class="fs-1 dropdown-transition"><a href="page7.php" class="text-decoration-none link-light vidaloka-nav">Trip
+                                    <li id="nav-href-4" class="fs-3 fs-sm-1 dropdown-transition"><a href="page7.php" class="text-decoration-none link-light vidaloka-nav">Trip
                                             Enhancements</a></li>
                                 </ul>
                             </div>
 
-                            <div class="col-5">
+                            <div class="col-12 col-sm-5">
                                 <ul class="list-unstyled dropdown-transition">
-                                    <li id="nav-href-5" class="fs-1 dropdown-transition"><a href="page5.php" class="text-decoration-none link-light vidaloka-nav" data-section="nav" data-value="gallery">Gallery</a></li>
-                                    <li id="nav-href-6" class="fs-1 dropdown-transition"><a href="page2.php" class="text-decoration-none link-light vidaloka-nav">FAQ </a></li>
-                                    <li id="nav-href-7" class="fs-1 dropdown-transition"><a href="page4.php" class="text-decoration-none link-light vidaloka-nav" data-section="nav" data-value="contact">Contact</a></li>
+                                    <li id="nav-href-5" class="fs-3 fs-sm-1 dropdown-transition"><a href="page5.php" class="text-decoration-none link-light vidaloka-nav" data-section="nav" data-value="gallery">Gallery</a></li>
+                                    <li id="nav-href-6" class="fs-3 fs-sm-1 dropdown-transition"><a href="page2.php" class="text-decoration-none link-light vidaloka-nav">FAQ </a></li>
+                                    <li id="nav-href-7" class="fs-3 fs-sm-1 dropdown-transition"><a href="page4.php" class="text-decoration-none link-light vidaloka-nav" data-section="nav" data-value="contact">Contact</a></li>
                                 </ul>
                                 <div id="nav-href-8" class="social-row display-5 nav col-md-5 justify-content-start ">
                                     <div class="soc-item ms-3">

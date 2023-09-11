a4c2950007f404caa28d327c34667e0c88cffa08
Maxi-Max10 <maxgiu3@gmail.com>
2023-09-11 19:06:54 -0300

    mail

--- page2.php
+++ page2.php
@@ -131,7 +131,7 @@ include "nav2.php";
                 
                 <h4 class="mbr-section-subtitle mbr-fonts-style mb-4 display-1" data-section="page2" data-value="preg6">Still need help?</h4>
                 <p class="mbr-text mbr-fonts-style display-7" data-section="page2" data-value="resp6">
-                    For any other questions, please contact us here <br> or write us at <strong> vivacsdelplata@gmail.com </strong></p>
+                    For any other questions, please contact us here <br> or write us at <strong> contact@vivacsdelplata.com </strong></p>
             </div>
         </div>
     </div>

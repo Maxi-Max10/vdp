f10d7ec524e1d8b8a68fc407023f5fe13c27b792
Maxi-Max10 <maxgiu3@gmail.com>
2023-09-11 18:18:54 -0300

    carrus

--- index.php
+++ index.php
@@ -7,38 +7,38 @@ include "nav2.php"
 
 <section data-bs-version="5.1" class="slider1 cid-tLdPgbAvY8 mb-0" id="slider1-1i">
     <div class="mbr-overlay" style="z-index: 2;"></div>
-    <div class="carousel slide carousel-fade" id="tLdXy8znRP" data-ride="carousel" data-bs-ride="carousel" data-interval="5000" data-bs-interval="5000">
+    <div class="carousel slide fadeInLeft" id="tLdXy8znRP" data-ride="carousel" data-bs-ride="carousel" data-interval="2000" data-bs-interval="2000">
 
         <div class="carousel-inner">
             <div class="carousel-item slider-image item active">
                 <div class="item-wrapper">
-                    <img class="d-block w-100" src="assets/images/3da8edbb-7dce-4e06-9786-17106bd337cc-1536x2049.jpg" alt="" data-slide-to="0" data-bs-slide-to="0">
+                    <img class="fadeInLeft w-100" src="assets/images/3da8edbb-7dce-4e06-9786-17106bd337cc-1536x2049.jpg" alt="" data-slide-to="0" data-bs-slide-to="0">
                 </div>
             </div>
             <div class="carousel-item slider-image item">
                 <div class="item-wrapper">
-                    <img class="d-block w-100" src="assets/images/3f3560a9-73e5-4745-bfe1-7f99d9e4e47d-1170x1560.jpg" alt="" data-slide-to="1" data-bs-slide-to="1">
+                    <img class="fadeInLeft w-100" src="assets/images/3f3560a9-73e5-4745-bfe1-7f99d9e4e47d-1170x1560.jpg" alt="" data-slide-to="1" data-bs-slide-to="1">
                 </div>
             </div>
             <div class="carousel-item slider-image item">
                 <div class="item-wrapper">
-                    <img class="d-block w-100" src="assets/images/8b393883-8a45-489a-a595-54baf00dc833.poster-1170x658.jpg" alt="" data-slide-to="2" data-bs-slide-to="2">
+                    <img class="fadeInLeft w-100" src="assets/images/8b393883-8a45-489a-a595-54baf00dc833.poster-1170x658.jpg" alt="" data-slide-to="2" data-bs-slide-to="2">
                 </div>
             </div>
             <div class="carousel-item slider-image item">
                 <div class="item-wrapper">
-                    <img class="d-block w-100" src="assets/images/2b944d58-6582-40f3-b1ba-f9e002065663-4-1537x2049.jpeg" alt="" data-slide-to="3" data-bs-slide-to="3">
+                    <img class="fadeInLeft w-100" src="assets/images/2b944d58-6582-40f3-b1ba-f9e002065663-4-1537x2049.jpeg" alt="" data-slide-to="3" data-bs-slide-to="3">
                 </div>
             </div>
             <div class="carousel-item slider-image item">
                 <div class="item-wrapper">
-                    <img class="d-block w-100" src="assets/images/img-4629-1170x1560.jpg" alt="" data-slide-to="4" data-bs-slide-to="5">
+                    <img class="fadeInLeft w-100" src="assets/images/img-4629-1170x1560.jpg" alt="" data-slide-to="4" data-bs-slide-to="5">
                 </div>
             </div>
         </div>
         <div class="carousel-item slider-image item">
             <div class="item-wrapper">
-                <img class="d-block w-100" src="assets/images/IMG_3644.jpeg" alt="" data-slide-to="5" data-bs-slide-to="6">
+                <img class="fadeInLeft w-100" src="assets/images/IMG_3644.jpeg" alt="" data-slide-to="5" data-bs-slide-to="6">
             </div>
         </div>
 
